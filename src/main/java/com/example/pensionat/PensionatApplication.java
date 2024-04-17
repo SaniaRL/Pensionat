d9f849cf4e880b61daf15a043d159cfc56df9d2c
SaniaRL <saniarunnfors@gmail.com>
2024-04-17 14:16:00 +0200

    Init Commit

---
+++ src/main/java/com/example/pensionat/PensionatApplication.java
@@ -0,0 +1,13 @@
+package com.example.pensionat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PensionatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PensionatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pensionat/PensionatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pensionat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PensionatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
