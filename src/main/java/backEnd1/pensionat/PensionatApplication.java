7a110916f741a5a6da0ce63933ff57f42b51e1cc
SaniaRL <saniarunnfors@gmail.com>
2024-04-17 20:58:00 +0200

    Fixed: Application not in default package

--- src/main/java/backEnd1/pensionat/PensionatApplication.java
+++ src/main/java/backEnd1/pensionat/PensionatApplication.java
@@ -1,4 +1,4 @@
-package com.example.pensionat;
+package backEnd1.pensionat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
