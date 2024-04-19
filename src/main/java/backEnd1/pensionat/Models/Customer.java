3f57b5442f4ee59867b4973a154d575ed794a3d3
SimNordlund <147151891+SimNordlund@users.noreply.github.com>
2024-04-19 12:47:44 +0200

    Created CustomerModel Test2

--- src/main/java/backEnd1/pensionat/Models/Customer.java
+++ src/main/java/backEnd1/pensionat/Models/Customer.java
@@ -1,10 +1,8 @@
 package backEnd1.pensionat.Models;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
