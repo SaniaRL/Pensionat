8107c9496ad9c93ee73335330c0c888db86e7ff3
SaniaRL <saniarunnfors@gmail.com>
2024-04-22 19:10:56 +0200

    Simple Booking - Booking without customer

---
+++ src/main/java/backEnd1/pensionat/dtos/SimpleBookingDTO.java
@@ -0,0 +1,23 @@
+package backEnd1.pensionat.dtos;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class SimpleBookingDTO {
+    private Long id;
+
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDate startDate;
+
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDate endDate;
+}
