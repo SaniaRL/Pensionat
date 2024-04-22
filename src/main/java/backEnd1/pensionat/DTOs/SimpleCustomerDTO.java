1a83381496951b995fa65ebdda1af401f3a68e89
SaniaRL <saniarunnfors@gmail.com>
2024-04-22 19:49:58 +0200

    Simple Customer

--- src/main/java/backEnd1/pensionat/DTOs/SimpleCustomerDTO.java
+++ src/main/java/backEnd1/pensionat/DTOs/SimpleCustomerDTO.java
@@ -1,19 +1,17 @@
-package backEnd1.pensionat.dtos;
+package backEnd1.pensionat.DTOs;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDate;
-
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class SimpleBookingDTO {
+public class SimpleCustomerDTO {
 
     private Long id;
-    private LocalDate startDate;
-    private LocalDate endDate;
+    private String name;
+    private String email;
 }
