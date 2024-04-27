d860c61cbdb780913400ed39cc98d0014e852c22
SimNordlund <147151891+SimNordlund@users.noreply.github.com>
2024-04-27 13:44:55 +0200

    Can update customer now, updating page needs styling and css. I also noticed that some methods in service classes needs adjustments to handle dtos correctly.

--- src/main/java/backEnd1/pensionat/Controllers/CustomerController.java
+++ src/main/java/backEnd1/pensionat/Controllers/CustomerController.java
@@ -29,14 +29,15 @@ public class CustomerController {
         return customerService.addCustomer(new Customer(name, email));
     }
 
-    @PostMapping("/addCustomerObject")
-    public String addCustomerObject(@RequestBody Customer customer) {
-        return customerService.addCustomer(customer);
-    }
-
     @DeleteMapping("/{id}/remove")
     public String removeCustomerById(@PathVariable Long id) {
         return customerService.removeCustomerById(id);
+    }
+
+    //Temp för att posta in kunder
+    @PostMapping("/addCustomerObject")
+    public String addCustomerObject(@RequestBody Customer customer) {
+        return customerService.addCustomer(customer);
     }
 
     //DeleteMapping verkar ej fungera? Testar Requestmapping
@@ -46,11 +47,27 @@ public class CustomerController {
         return handleCustomers(model);
     }
 
-    @PutMapping("/{id}/update")
-    public String updateCustomer(@PathVariable Long id, @RequestBody Customer customer) {
-        customerService.updateCustomer(customer);
-        return "index";
+    //Skapa senare upp en för ID vid behov.
+    @RequestMapping("/{email}/update")
+    public String updateCustomerHandler(@PathVariable String email, Model model){
+        Customer c = customerService.getCustomerByEmail(email);
+        model.addAttribute("kund", c);
+        return "updateCustomers.html";
     }
+
+    //Temp också
+    @PostMapping("/handle/update")
+    public String handleCustomersUpdate(Model model, Customer customer){
+        customerService.addCustomer(customer);
+        int currentPage = 1;
+        Page<SimpleCustomerDTO> c = customerService.getAllCustomersPage(currentPage);
+        model.addAttribute("allCustomers", c.getContent());
+        model.addAttribute("currentPage", currentPage);
+        model.addAttribute("totalItems", c.getTotalElements());
+        model.addAttribute("totalPages", c.getTotalPages());
+        return "handleCustomers.html";
+    }
+
 
     @RequestMapping("/customerOrNot")
     public String loadCustomerOrNot(){
