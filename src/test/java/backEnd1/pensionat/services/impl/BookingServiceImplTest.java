5b54ae9ea2f6e64da3b0f82d3877ce83548198ab
”Eddie <eddie.boschek@gmail.com>
2024-05-01 18:14:22 +0200

    AddBookingFromBookingDto unit test, getBookingById unit test and removeBookingById unit test added.

--- src/test/java/backEnd1/pensionat/services/impl/BookingServiceImplTest.java
+++ src/test/java/backEnd1/pensionat/services/impl/BookingServiceImplTest.java
@@ -1,5 +1,7 @@
 package backEnd1.pensionat.services.impl;
 
+import backEnd1.pensionat.DTOs.BookingDTO;
+import backEnd1.pensionat.DTOs.CustomerDTO;
 import backEnd1.pensionat.DTOs.DetailedBookingDTO;
 import backEnd1.pensionat.DTOs.SimpleCustomerDTO;
 import backEnd1.pensionat.Models.Booking;
@@ -19,6 +21,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
@@ -47,9 +50,11 @@ class BookingServiceImplTest {
     LocalDate endDate = LocalDate.parse("2024-05-17");
 
     Customer customer = new Customer(name, email);
+    CustomerDTO customerDto = new CustomerDTO(name, email);
     SimpleCustomerDTO simpleCustomerDTO = new SimpleCustomerDTO(id, name, email);
     Booking booking = new Booking(customer, startDate, endDate);
     DetailedBookingDTO detailedBookingDTO = new DetailedBookingDTO(id, simpleCustomerDTO, startDate, endDate);
+    BookingDTO bookingDto = new BookingDTO(customerDto, startDate, endDate);
 
     @Test
     void getAllBookings() {
@@ -65,23 +70,44 @@ class BookingServiceImplTest {
         booking.setId(id);
         when(bookingRepo.save(any(Booking.class))).thenReturn(booking);
         BookingServiceImpl service = new BookingServiceImpl(bookingRepo, customerRepo, customerService,
-                roomService, orderLineService);
+                                                            roomService, orderLineService);
         DetailedBookingDTO actual = service.addBooking(detailedBookingDTO);
         assertEquals(actual.getId(), detailedBookingDTO.getId());
         assertEquals(actual.getStartDate(), detailedBookingDTO.getStartDate());
         assertEquals(actual.getEndDate(), detailedBookingDTO.getEndDate());
     }
 
+
     @Test
     void addBookingFromBookingDto() {
+        when(customerRepo.findByEmail(booking.getCustomer().getEmail())).thenReturn(customer);
+        when(bookingRepo.save(any(Booking.class))).thenAnswer(invocation -> {
+            Booking savedBooking = invocation.getArgument(0);
+            savedBooking.setId(1L);
+            return savedBooking;
+        });
+        BookingServiceImpl service = new BookingServiceImpl(bookingRepo, customerRepo, customerService,
+                                                            roomService, orderLineService);
+        Long actual = service.addBookingFromBookingDto(bookingDto);
+        assertNotNull(actual);
     }
 
     @Test
     void getBookingById() {
+        booking.setId(id);
+        when(bookingRepo.findById(id)).thenReturn(Optional.of(booking));
+        BookingServiceImpl service = new BookingServiceImpl(bookingRepo, customerRepo, customerService,
+                                                            roomService, orderLineService);
+        DetailedBookingDTO actual = service.getBookingById(id);
+        assertEquals(actual.getId(), id);
     }
 
     @Test
     void removeBookingById() {
+        BookingServiceImpl service = new BookingServiceImpl(bookingRepo, customerRepo, customerService,
+                                                            roomService, orderLineService);
+        String feedback = service.removeBookingById(id);
+        assertTrue(feedback.equalsIgnoreCase("Booking removed successfully"));
     }
 
     @Test
