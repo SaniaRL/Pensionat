d9c41bf092e35265d2304da67826558530da3372
”Eddie <eddie.boschek@gmail.com>
2024-05-02 13:41:03 +0200

    All unit tests for OrderLineController added.

---
+++ src/test/java/com/example/pensionat/controllers/OrderLineControllerTest.java
@@ -0,0 +1,83 @@
+package com.example.pensionat.controllers;
+
+import com.example.pensionat.dtos.*;
+import com.example.pensionat.enums.RoomType;
+import com.example.pensionat.models.Booking;
+import com.example.pensionat.models.Customer;
+import com.example.pensionat.models.OrderLine;
+import com.example.pensionat.models.Room;
+import com.example.pensionat.repositories.OrderLineRepo;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+import java.time.LocalDate;
+import java.util.Arrays;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class OrderLineControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private OrderLineRepo mockRepo;
+
+    @BeforeEach
+    public void init() {
+        Room room1 = new Room(201L, 0, null);
+
+        String name = "Maria";
+        String email = "maria@mail.com";
+        LocalDate startDate = LocalDate.parse("2024-05-14");
+        LocalDate endDate = LocalDate.parse("2024-05-17");
+
+        Customer customer = new Customer(name, email);
+        Booking booking = new Booking(1L, customer, startDate, endDate, null);
+        Long orderLineId = 3L;
+        OrderLine orderLine = new OrderLine(orderLineId, booking, room1, 2);
+
+        when(mockRepo.findAll()).thenReturn(Arrays.asList(orderLine));
+    }
+
+    @Test
+    void getAllOrderLines() throws Exception {
+        this.mvc.perform(get("/orderline/all"))
+                .andExpect(status().isOk())
+                .andExpect(content().json("[{\"bookingId\":1, \"room\": { \"id\":201 ,\"roomType\": \"SINGLE\"}," +
+                                            "\"extraBeds\":2}]"));
+    }
+
+    @Test
+    void addOrderLine() throws Exception {
+        this.mvc.perform(post("/orderline/add")
+                        .param("name", "Holger Persson")
+                        .param("email", "holger@email.com")
+                        .param("startDate", "2024-06-14")
+                        .param("endDate", "2024-06-20")
+                        .param("roomId", "302")
+                        .param("typeOfRoom", "0")
+                        .param("extraBeds", "0"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("OrderLine added")));
+    }
+
+    @Test
+    void removeOrderLineById() throws Exception {
+        this.mvc.perform(get("/orderline/201/remove"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("Room 201 removed")));
+    }
+}
