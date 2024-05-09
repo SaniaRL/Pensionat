4843fbe3277f2b9689dded3825d2512c72dc8429
SimNordlund <147151891+SimNordlund@users.noreply.github.com>
2024-05-09 09:34:13 +0200

    Logic from FetchShippers moved to serviceclass

--- src/main/java/com/example/pensionat/services/impl/ShippersServiceImpl.java
+++ src/main/java/com/example/pensionat/services/impl/ShippersServiceImpl.java
@@ -1,23 +1,36 @@
 package com.example.pensionat.services.impl;
 
-import com.example.pensionat.dtos.RoomDTO;
+import com.example.pensionat.dtos.DetailedShippersDTO;
 import com.example.pensionat.dtos.SimpleShippersDTO;
-import com.example.pensionat.models.Room;
 import com.example.pensionat.models.Shippers;
 import com.example.pensionat.repositories.ShippersRepo;
-import com.example.pensionat.services.convert.RoomTypeConverter;
 import com.example.pensionat.services.convert.ShippersConverter;
 import com.example.pensionat.services.interfaces.ShippersService;
 import org.springframework.stereotype.Service;
 
+
 @Service
 public class ShippersServiceImpl implements ShippersService {
 
-    ShippersRepo shippersRepo;
+    private final ShippersRepo shippersRepo;
+
+    ShippersServiceImpl(ShippersRepo shippersRepo) {
+        this.shippersRepo = shippersRepo;
+    }
 
     @Override
     public SimpleShippersDTO getShippersById(Long id) {
         Shippers shipper = shippersRepo.findById(id).orElse(null);
         return ShippersConverter.shippersToSimpleShippersDTO(shipper);
+    }
+
+    @Override
+    public String saveDownAllShippersToDB(DetailedShippersDTO[] shippersArray) {
+        DetailedShippersDTO[] detailedShippersList = shippersArray;
+        for (DetailedShippersDTO tempShipper : detailedShippersList) {
+            Shippers convertedShipper = ShippersConverter.detailedShippersDTOToShippers(tempShipper);
+            shippersRepo.save(convertedShipper);
+        }
+        return "Alla fraktföretag sparades ner";
     }
 }
