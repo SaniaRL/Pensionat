91ef8d2c03ab21a08aee66e08049926a64b785cf
”Eddie <eddie.boschek@gmail.com>
2024-05-22 17:03:18 +0200

    UserService interface created

--- src/main/java/com/example/pensionat/controllers/UserController.java
+++ src/main/java/com/example/pensionat/controllers/UserController.java
@@ -1,8 +1,12 @@
 package com.example.pensionat.controllers;
 
+import com.example.pensionat.repositories.UserRepo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
@@ -10,4 +14,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class UserController {
 
     private final UserRepo userRepo;
+
+    @GetMapping("/")
+    public String handleCustomers(Model model){
+        int currentPage = 1;
+        customerService.addToModel(currentPage, model);
+        return "handleCustomers";
+    }
+
+    @GetMapping(value = "/", params = "page")
+    public String handleByPage(Model model, @RequestParam int page){
+        customerService.addToModel(page, model);
+        return "handleCustomers";
+    }
 }
---
+++ src/main/java/com/example/pensionat/services/interfaces/UserService.java
@@ -0,0 +1,10 @@
+package com.example.pensionat.services.interfaces;
+
+import com.example.pensionat.dtos.SimpleCustomerDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.ui.Model;
+
+public interface UserService {
+    void addToModel(int currentPage, Model model);
+    Page<SimpleUserDTO> getAllCustomersPage(int pageNum);
+}
