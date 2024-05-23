10ad1387cebb4e4390ddc80d018e7f6b460e7bf0
SaniaRL <saniarunnfors@gmail.com>
2024-05-23 21:33:19 +0200

    Login w feedback

--- src/main/java/com/example/pensionat/controllers/AuthController.java
+++ src/main/java/com/example/pensionat/controllers/AuthController.java
@@ -3,11 +3,23 @@ package com.example.pensionat.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AuthController {
-    @RequestMapping("/login")
+/*
+@RequestMapping("/login")
     public String login() {
+        return "login";
+    }
+
+ */
+
+    @RequestMapping("/login")
+    public String loginPage(Model model, @RequestParam(value = "error", required = false) String error) {
+        if (error != null) {
+            model.addAttribute("loginError", true);
+        }
         return "login";
     }
 
@@ -16,4 +28,5 @@ public class AuthController {
         model.addAttribute("loginError", true);
         return "login";
     }
+
 }
