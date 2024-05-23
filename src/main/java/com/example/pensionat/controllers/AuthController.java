5fc47c69d289330fecdad2afe06e24469ee33644
SaniaRL <saniarunnfors@gmail.com>
2024-05-23 21:50:17 +0200

    Glömt lösenord? works

--- src/main/java/com/example/pensionat/controllers/AuthController.java
+++ src/main/java/com/example/pensionat/controllers/AuthController.java
@@ -16,9 +16,14 @@ public class AuthController {
  */
 
     @RequestMapping("/login")
-    public String loginPage(Model model, @RequestParam(value = "error", required = false) String error) {
+    public String loginPage(Model model,
+                            @RequestParam(value = "error", required = false) String error,
+                            @RequestParam(value = "newPassword", required = false) String newPassword) {
         if (error != null) {
             model.addAttribute("loginError", true);
+        }
+        if (newPassword != null) {
+            model.addAttribute("newPassword", true);
         }
         return "login";
     }
