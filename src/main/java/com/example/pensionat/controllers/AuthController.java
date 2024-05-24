e14e5cd8e0be4b1581369c3bf448681f27bffc4b
SaniaRL <saniarunnfors@gmail.com>
2024-05-24 20:32:10 +0200

    Ethereal.email properties

--- src/main/java/com/example/pensionat/controllers/AuthController.java
+++ src/main/java/com/example/pensionat/controllers/AuthController.java
@@ -1,13 +1,19 @@
 package com.example.pensionat.controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
+//import org.springframework.mail.javamail.JavaMailSender;
 @Controller
 public class AuthController {
+
+//    @Autowired
+  //  private JavaMailSender emailSender;
 
     @RequestMapping("/login")
     public String loginPage(Model model,
@@ -26,6 +32,13 @@ public class AuthController {
     public String loginError(Model model) {
         model.addAttribute("loginError", true);
         return "login";
+    }
+
+    @PostMapping("/forgot-password")
+    public String forgotPassword(@RequestParam("email") String email, HttpServletRequest request) {
+
+
+        return "redirect:/login?resetPassword";
     }
 
 }
