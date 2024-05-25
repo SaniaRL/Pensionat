05539da8a714310b2f0f091fa20927ad102d775f
SaniaRL <saniarunnfors@gmail.com>
2024-05-25 20:20:32 +0200

    Works and sends out a mail but does not get mail from properties

--- src/main/java/com/example/pensionat/controllers/AuthController.java
+++ src/main/java/com/example/pensionat/controllers/AuthController.java
@@ -3,15 +3,21 @@ package com.example.pensionat.controllers;
 import com.example.pensionat.dtos.SimpleUserDTO;
 import com.example.pensionat.models.User;
 import com.example.pensionat.services.interfaces.UserService;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Controller
@@ -21,8 +27,12 @@ public class AuthController {
     private final JavaMailSender emailSender;
     private final UserService userService;
 
+    //TODO hämta från properties
+    /*
     @Value("${spring.mail.username}")
-    private String fromEmail;
+    */
+    private final String fromEmail = "dominique.wiegand@ethereal.email";
+
 
     @RequestMapping("/login")
     public String loginPage(Model model,
@@ -84,9 +94,86 @@ public class AuthController {
         return "login";
     }
 
+
+    @PostMapping("/forgotPassword-24")
+    public String forgotPassword24(@RequestParam(value="mail", required = false) String mail, Model model) {
+
+        //Reset token
+        String resetToken = generateResetPasswordToken(mail);
+        //TODO: Lagra reset token på lämpligt sätt
+
+        String resetLink = "localhost:8080/resetPassword?token=" + resetToken;
+
+        //TODO Hämta mall och shit men asså
+        String subject = "Återställ lösenord";
+        String message = "<div>Följ denna <a href=" + resetLink + ">länk</a></div>";
+
+        /*
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setFrom(fromEmail);
+        mailMessage.setTo(mail);
+        mailMessage.setSubject(subject);
+        mailMessage.setText(message);
+
+        emailSender.send(mailMessage);
+
+         */
+
+        try {
+            MimeMessage mimeMessage = emailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "utf-8");
+            helper.setText(message, true);
+            helper.setTo(mail);
+            helper.setSubject(subject);
+            helper.setFrom(fromEmail);
+            emailSender.send(mimeMessage);
+        } catch (MessagingException e) {
+            model.addAttribute("mailError", true);
+            return "login";
+        }
+
+        //Uppdatera GUI (Kanske skulle förvara texten här bak och kunna ha flera text idk)
+        model.addAttribute("mailSent", true);
+
+        return "login";
+    }
+
+    @GetMapping("/resetPassword")
+    public String resetPassword(@RequestParam(value = "token") String token, Model model) {
+
+        String[] tokenArray = token.split("\\|");
+        if (tokenArray.length != 3) {
+            //TODO fixa detta chill idk
+            System.out.println("error token != 3");
+            System.out.println(tokenArray.length);
+            System.out.println(token);
+            model.addAttribute("tokenError", true);
+            return "index";
+        }
+
+        //TODO Måste kolla av allt annat också - eller tid iaf
+
+        String username = tokenArray[1];
+        model.addAttribute("username", username);
+
+        return "resetPassword";
+    }
+
+    @PostMapping("/updatePassword")
+    public String updatePassword(@RequestParam("token") String token, @RequestParam("password") String password, Model model) {
+
+        return "login";
+    }
+
     //TODO inte en service - en util? Var ska denna metod bo?
 
-    static String generateOTP(int length)
+    private String generateResetPasswordToken(String username) {
+        String token = UUID.randomUUID().toString();
+        LocalDateTime timestamp = LocalDateTime.now();
+        return token + "%7C" + username + "%7C" + timestamp;
+    }
+
+    private String generateOTP(int length)
     {
         String str = "abcdefghijklmnopqrstuvwxyzABCD"
                 +"EFGHIJKLMNOPQRSTUVWXYZ0123456789-";
