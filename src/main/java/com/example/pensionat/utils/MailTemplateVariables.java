2b95e02ec2e115995da4c48b31dbe983ccf987df
SaniaRL <saniarunnfors@gmail.com>
2024-05-27 15:02:27 +0200

    correct color active template

--- src/main/java/com/example/pensionat/utils/MailTemplateVariables.java
+++ src/main/java/com/example/pensionat/utils/MailTemplateVariables.java
@@ -24,17 +24,4 @@ public class MailTemplateVariables {
         return Arrays.asList("Bekräfta Bokning", "Återställ Lösenord");
     }
 
-    //TODO verkar bli tight med detta elände:
-    /*
-    public static Context getBookingConfirmationContext(BookingData bd) {
-        Context context = new Context();
-        context.setVariable("Namn", bd.getName());
-        context.setVariable("E-post", bd.getEmail());
-        context.setVariable("Startdatum", bd.getStartDate());
-        context.setVariable("Slutdatum", bd.getEndDate());
-        context.setVariable("orderLines", bd.getChosenRooms());
-        return context;
-    }
-
-     */
 }
